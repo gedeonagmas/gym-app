8b1d884c73636ee4e48dd5c91cf71551940376c4
gedeonagmas <gedeonagmas2580@gmail.com>
2023-11-10 08:51:38 +0300

    first commit

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,35 @@
+import About from "../components/About/About";
+import BmiCalc from "../components/BMI/BmiCalc";
+import Blog from "../components/Blog/Blog";
+import ChooseUs from "../components/ChooseUs/ChooseUs";
+import CtaBanner from "../components/CtaBanner/CtaBanner";
+import Featured from "../components/Featured/Featured";
+import Footer from "../components/Footer/Footer";
+import Gallery from "../components/Gallery/Gallery";
+import Hero from "../components/Hero/Hero";
+import Testimonials from "../components/Testimonials/Testimonials";
+import Trainers from "../components/Trainers/Trainers";
+import Pricing from "../components/Pricing/Pricing";
+
+function Home() {
+  window.scrollTo({ top: 0 });
+
+  return (
+    <>
+      <Hero />
+      <About />
+      <Featured />
+      <ChooseUs />
+      <Trainers />
+      <Testimonials />
+      <Gallery />
+      <BmiCalc />
+      <Pricing />
+      <Blog />
+      <CtaBanner />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
